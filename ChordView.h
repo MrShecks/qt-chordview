64a879336d9834636a55ea5f031426ca67de89ea
shecks <shecks@gmail.com>
2020-04-12 19:30:54 +0100

    Initial commit

---
+++ ChordView.h
@@ -0,0 +1,40 @@
+/*
+ * File: ChordView.h
+ * Created: 2020-4-12
+ *
+ * Copyright (c) shecks 2020 <shecks@gmail.com>
+ * All rights reserved.
+ *
+ * This file is part of %QT_PROJECT_NAME%.
+ *
+ * %QT_PROJECT_NAME% is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * %QT_PROJECT_NAME% is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with %QT_PROJECT_NAME%.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef CHORDVIEW_H
+#define CHORDVIEW_H
+
+#include <QWidget>
+
+class ChordView : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ChordView(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // CHORDVIEW_H
