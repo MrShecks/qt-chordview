465e44fc5be78a05efccdf81c913a7cab16cffd2
shecks <shecks@gmail.com>
2020-04-12 16:06:14 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+/*
+ * File: %{Cpp:License:FileName}
+ * Created: 2020-4-12
+ *
+ * Copyright (c) shecks 2020 <shecks@gmail.com>
+ * All rights reserved.
+ *
+ * This file is part of %QT_PROJECT_NAME%.
+ *
+ * %QT_PROJECT_NAME% is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * %QT_PROJECT_NAME% is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with %QT_PROJECT_NAME%.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "ChordViewDialog.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    ChordViewDialog dlg;
+
+    dlg.show();
+
+    return app.exec();
+}
